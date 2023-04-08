e991ee03bb0c5da6cf5d220dfa9efcda9bc29364
Ela Aydoğdu <elayd.gg@gmail.com>
2023-04-08 23:19:08 +0300

    collection.
    iteme yaklaşıldığında collect butonu ortaya çıkar.

---
+++ Assets/Scripts/ItemCollection.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollection : MonoBehaviour
+{
+    public Button collectButton;
+
+    private void Start()
+    {
+            collectButton.gameObject.SetActive(false);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collectButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            collectButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void Collect()
+    {
+        Destroy(gameObject);
+        collectButton.gameObject.SetActive(false);
+    }
+
+}
