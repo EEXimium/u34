930d8026a4cc3c9a19cc47f72222078fe483850d
Yağız Efe Atasever <ataseverefeyagiz@gmail.com>
2023-04-09 14:52:34 +0300

    dosya düzenleme

--- Assets/Scripts/ItemCollection.cs
+++ Assets/Scripts/ItemCollection.cs
@@ -9,7 +9,6 @@ public class ItemCollection : MonoBehaviour
 
     private void Start()
     {
-            //buton görünmez olur, mouse simgesi ekranda belirir.
             collectButton.gameObject.SetActive(false);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
@@ -17,25 +16,18 @@ public class ItemCollection : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        ////karakterle item etkileþime girdiðinde buton ortaya çýkar.
         if (other.CompareTag("Player"))
-        {
             collectButton.gameObject.SetActive(true);
-        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //karakter ve itemin etkileþimi kesildiðinde buton ortadan kalkar.
         if (other.CompareTag("Player"))
-        {
             collectButton.gameObject.SetActive(false);
-        }
     }
 
     public void Collect()
     {
-        //istenilen nesne yok edilir, buton tekrar görünmez hale gelir.
         Destroy(gameObject);
         collectButton.gameObject.SetActive(false);
     }
